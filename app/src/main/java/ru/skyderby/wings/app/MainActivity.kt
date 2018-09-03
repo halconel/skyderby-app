f2da8d6b649861bf57bf75ad12713dc7ee900ede
Dmitry Vinogradov <di.vinogradov@gmail.com>
2018-09-04 02:17:34 +0300

    Authentication (Not finished)

--- app/src/main/java/ru/skyderby/wings/app/MainActivity.kt
+++ app/src/main/java/ru/skyderby/wings/app/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.skyderby.wings.app
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebViewDatabase
 
 import com.basecamp.turbolinks.TurbolinksSession
 import com.basecamp.turbolinks.TurbolinksAdapter
@@ -10,8 +12,9 @@ import com.basecamp.turbolinks.TurbolinksView
 
 class MainActivity : AppCompatActivity(), TurbolinksAdapter {
     // Change the BASE_URL to an address that your VM or device can hit.
-    private val BASE_URL = "https://skyderby.ru/?mobile=1"
-    private val INTENT_URL = "https://skyderby.ru/?mobile=1"
+    private  val HOST = "skyderby.ru"
+    private val BASE_URL = "https://" + HOST + "/?mobile=1"
+    private val INTENT_URL = "https://" + HOST + "/?mobile=1"
 
     private var location: String? = null
     private var turbolinksView: TurbolinksView? = null
@@ -28,15 +31,28 @@ class MainActivity : AppCompatActivity(), TurbolinksAdapter {
         val mEmail = intent.getStringExtra(USER_NAME)
         val mPassword = intent.getStringExtra(PASSWORD)
         val mToken = intent.getStringExtra(TOKEN)
-        if (mToken == null) {
+        if (mToken == null || mEmail == null || mPassword == null) {
             // The credentials is empty
-
         } else {
+            WebViewDatabase.getInstance(this).clearHttpAuthUsernamePassword()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                WebViewDatabase
+                        .getInstance(this)
+                        .setHttpAuthUsernamePassword(HOST, HOST, mEmail, mPassword)
+            } else {
+                TurbolinksSession.getDefault(this)
+                        .webView
+                        .setHttpAuthUsernamePassword(HOST, HOST, mEmail, mPassword)
+            }
+       }
 
-        }
+        TurbolinksSession.getDefault(this)
+                .webView
+                .settings
+                .setUserAgentString("turbolinks-view\\inventory-android")
 
         // Find the custom TurbolinksView object in your layout
-        turbolinksView = findViewById<TurbolinksView>(R.id.turbolinks_view)
+        turbolinksView = findViewById(R.id.turbolinks_view)
 
         // For this example we set a default location, unless one is passed in through an intent
         location = if (intent.getStringExtra(INTENT_URL) != null) intent.getStringExtra(INTENT_URL) else BASE_URL
