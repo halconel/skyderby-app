72ba42b8f28d6f1ae8fdf86f8e6b0eefb2e314e5
Dmitry Vinogradov <di.vinogradov@gmail.com>
2018-10-04 02:17:09 +0300

    Splash screen tuned
    + bug-fix: fixed issue with rerun the application

--- app/src/main/java/ru/skyderby/wings/app/activities/SplashActivity.kt
+++ app/src/main/java/ru/skyderby/wings/app/activities/SplashActivity.kt
@@ -2,6 +2,7 @@ package ru.skyderby.wings.app.activities
 
 import android.app.Activity
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import ru.skyderby.wings.app.R
@@ -10,14 +11,14 @@ import ru.skyderby.wings.app.helpers.PreferenceSave
 import java.io.IOException
 import androidx.core.app.ActivityOptionsCompat
 import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_splash.*
+import retrofit2.Response
+import ru.skyderby.wings.app.api.CredentialsMessage
 
 
 class SplashActivity : Activity() {
-    // Sky Derby API interface
-    private val skyDerbyApiServe by lazy {
-        SkyDerbyApiService.create()
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -26,44 +27,17 @@ class SplashActivity : Activity() {
         android.webkit.CookieManager.getInstance().setAcceptCookie(true)
         java.net.CookieHandler.setDefault(SkyDerbyApiService.proxy)
 
-        Handler().postDelayed({
-            // Get authorization credentials
-            val username = PreferenceSave.getInstance(this).login
-            val password = PreferenceSave.getInstance(this).password
+        // Get authorization credentials
+        val username = PreferenceSave.getInstance(this).login
+        val password = PreferenceSave.getInstance(this).password
 
-            if(username == "") {
-                // This is fist app start as well
-                startLoginActivity()
-            }
-            else {
-                // Credentials
-                val token = getAuthToken(username, password)
-                // Request user data to the sky derby API
-                try {
-                    val response = skyDerbyApiServe.getProfile(token).execute()
-                    if (response.isSuccessful) {
-                        // Authorized
-                        startMainActivity(username, password, token)
-                    }
-                    else {
-                        // Unauthorized
-                        startLoginActivity(username, password, response.code())
-                    }
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-
-            // close this activity
-            finish()
-        }, SPLASH_TIME_OUT.toLong())
+        val mAuthTask = UserLoginTask(username, password)
+        mAuthTask!!.execute(null as Void?)
     }
 
-    private fun startMainActivity(username: String?, password: String?, token: String?) {
+    private fun startMainActivity(profile: CredentialsMessage?) {
         val intent = Intent(this, MainActivity::class.java).apply {
-            putExtra(getString(R.string.USER_NAME), username)
-            putExtra(getString(R.string.PASSWORD), password)
-            putExtra(getString(R.string.TOKEN), token)
+            putExtra(getString(R.string.PROFILE), profile)
         }
         startActivity(intent)
     }
@@ -75,20 +49,42 @@ class SplashActivity : Activity() {
         startActivity(intent, options.toBundle())
     }
 
-    private fun startLoginActivity(username:String, password:String, code:Int) {
-        val intent = Intent(this, LoginActivity::class.java).apply {
-            putExtra(getString(R.string.USER_NAME), username)
-            putExtra(getString(R.string.PASSWORD), password)
-            putExtra(getString(R.string.RESPONSE_CODE), code)
-        }
-        val options = ActivityOptionsCompat
-                .makeSceneTransitionAnimation(this, imgLogo as View, "logo")
-        startActivity(intent, options.toBundle())
+    private val skyDerbyApiService by lazy {
+        SkyDerbyApiService.create()
     }
 
-    private fun getAuthToken(username:String, password:String): String {
-        val authHeader = ("$username:$password").toByteArray()
-        return "Basic " + android.util.Base64.encodeToString(authHeader, android.util.Base64.NO_WRAP)
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    inner class UserLoginTask internal constructor(
+            private val mEmail: String,
+            private val mPassword: String
+    ) : AsyncTask<Void, Void, Boolean>() {
+
+        var profileApiMessage: Response<CredentialsMessage>? = null
+
+        override fun doInBackground(vararg params: Void): Boolean? {
+            // Credentials
+            val authHeader = ("$mEmail:$mPassword").toByteArray()
+            val token = "Basic ${android.util.Base64
+                    .encodeToString(authHeader, android.util.Base64.NO_WRAP)}"
+            // Request user data to the sky derby API
+            try {
+                profileApiMessage = skyDerbyApiService.getProfile(token).execute()
+                return profileApiMessage!!.isSuccessful
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            return false
+        }
+
+        override fun onPostExecute(success: Boolean?) {
+            if (success!!) startMainActivity(profileApiMessage?.body())
+            else startLoginActivity()
+            finish()
+        }
     }
 
     companion object {
